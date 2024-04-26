0f7a27fc243612d0a8b7a9b5ba97425705f6d391
mmasias@macbook <manuel@masiasweb.com>
2024-04-26 21:00:27 +0200

    Introducción de Jugador y lógica de juego
    - Creación de la clase Jugador con esqueleto básico.
    - Añadido constructor de Jugador que acepta un nombre.
    - Implementado método juegaCon en Jugador para interactuar con otro jugador.
    - Método partidasGanadas agregado para obtener el número de partidas ganadas por el jugador.
    - Añadida funcionalidad verEstadisticas en Jugador para visualizar las estadísticas del jugador.
    - Modificación en PiedraPapelTijera para incluir un arreglo de jugadores con nombres 'Pepito' y 'Juanito'.
    - Cambio en el método jugar para implementar la lógica que determina el ganador basado en partidas ganadas.
    - Llamada a verEstadisticas para cada jugador tras el final de la partida.

---
+++ src/v000/Jugador.java
@@ -0,0 +1,21 @@
+package v000;
+
+public class Jugador {
+
+    public Jugador(String nombre) {
+
+    }
+
+    public void juegaCon(Jugador jugador) {
+
+    }
+
+    public int partidasGanadas() {
+
+    }
+
+    public void verEstadisticas() {
+
+    }
+
+}
--- src/v000/PiedraPapelTijera.java
+++ src/v000/PiedraPapelTijera.java
@@ -1,7 +1,19 @@
-class PiedraPapelTijera{
+class PiedraPapelTijera {
 
-    public void jugar(){
+    private Jugador[] jugadores;
 
+    public PiedraPapelTijera() {
+        jugadores = new Jugador[2];
+        jugadores[0] = new Jugador("Pepito");
+        jugadores[1] = new Jugador("Juanito");
+    }
+
+    public void jugar() {
+        do {
+            jugadores[0].juegaCon(jugadores[1]);
+        } while (jugadores[0].partidasGanadas() == 3 || jugadores[1].partidasGanadas() == 3);
+        jugadores[0].verEstadisticas();
+        jugadores[1].verEstadisticas();
     }
 
     public static void main(String[] args) {
