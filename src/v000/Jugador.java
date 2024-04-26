dc98933203d4bc86c1bc776174d1fd05b8450dda
mmasias@macbook <manuel@masiasweb.com>
2024-04-26 21:07:23 +0200

    Implementando juegaCon, mi sentido arácnido detecta repetición. Commiteo esto para la posteridad académica y en un momento lo arreglo...

--- src/v000/Jugador.java
+++ src/v000/Jugador.java
@@ -8,6 +8,24 @@ public class Jugador {
 
     public void juegaCon(Jugador jugador) {
 
+        int miJugada = this.lanzamiento();
+        int suJugada = jugador.lanzamiento();
+        String descripcion = "";
+
+        if(miJugada>suJugada){
+            this.gana();
+            descripcion = "Gano " + this.nombre;
+        } else if(miJugada<suJugada){
+            jugador.gana();
+            descripcion = "Gano " + jugador.nombre;
+        } else {
+            descripcion = "Empate!";
+        }
+        System.out.println(descripcion);
+    }
+
+    private int lanzamiento() {
+        return (int) (Math.random * 3);
     }
 
     public int partidasGanadas() {
