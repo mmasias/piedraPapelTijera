44cea4ef3a80b051761f25747f465e0f78e2d0d9
mmasias@macbook <manuel@masiasweb.com>
2024-04-26 21:59:41 +0200

    v000

--- src/v000/Jugador.java
+++ src/v000/Jugador.java
@@ -2,7 +2,19 @@ package v000;
 
 public class Jugador {
 
+    private int puntos;
+    private String nombre;
+    private String[] piezas;
+    private int[][] reglas = {
+            { 0, 1, 1 },
+            { -1, 0, -1 },
+            { -1, 1, 0 }
+    };
+
     public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.puntos = 0;
+        this.piezas = new String[] { "Piedra", "Papel", "Tijera" };
 
     }
 
@@ -10,36 +22,41 @@ public class Jugador {
 
         int miJugada = this.lanzamiento();
         int suJugada = jugador.lanzamiento();
-        String descripcion = "";
-     
-        if(miJugada>suJugada){
+
+        System.out.println(
+                "[" + this.nombre + "] saca [" + piezas[miJugada] + "] / [" + jugador.nombre + "] saca [" + piezas[suJugada] + "]");
+
+        int ganador = reglas[miJugada][suJugada];
+
+        if (ganador == 1) {
             this.gana();
-        } else if(miJugada<suJugada){
+        } else if (ganador == -1) {
             jugador.gana();
         } else {
             jugador.empate();
-        }  
+        }
     }
 
     private void empate() {
         System.out.println("Empate");
     }
 
-    private void gana(){
+    private void gana() {
         this.puntos++;
         System.out.println("Gana " + this.nombre);
     }
 
     private int lanzamiento() {
-        return (int) (Math.random * 3);
+        return (int) (Math.random() * 3);
     }
 
     public int partidasGanadas() {
-
+        return this.puntos;
     }
 
     public void verEstadisticas() {
-
+        System.out.println("Jugador " + nombre);
+        System.out.println(puntos + " puntos...");
     }
 
 }
--- src/v000/PiedraPapelTijera.java
+++ src/v000/PiedraPapelTijera.java
@@ -1,17 +1,19 @@
+package v000;
+
 class PiedraPapelTijera {
 
     private Jugador[] jugadores;
 
     public PiedraPapelTijera() {
         jugadores = new Jugador[2];
-        jugadores[0] = new Jugador("Pepito");
-        jugadores[1] = new Jugador("Juanito");
+        jugadores[0] = new Jugador("Bill Gates");
+        jugadores[1] = new Jugador("Steve Jobs");
     }
 
     public void jugar() {
         do {
             jugadores[0].juegaCon(jugadores[1]);
-        } while (jugadores[0].partidasGanadas() == 3 || jugadores[1].partidasGanadas() == 3);
+        } while (jugadores[0].partidasGanadas() < 3 && jugadores[1].partidasGanadas() < 3);
         jugadores[0].verEstadisticas();
         jugadores[1].verEstadisticas();
     }
